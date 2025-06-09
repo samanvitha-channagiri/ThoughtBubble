25aba1ee98b3d600fb82a31b6113cfc894a5d9c9
samanvitha-channagiri <samanvithachanna@gmail.com>
2025-06-09 15:24:37 +0530

    merge conflict issues resolved

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -1,18 +1,9 @@
 import mongoose from 'mongoose'
 export const connectDB=async()=>{
-<<<<<<< HEAD
     try{
     const conn=    await mongoose.connect(process.env.MONGO_URI)
-    console.log("db connected successfully"+conn.connection.user);
+  
     }catch(error){
         console.log("Error connecting db",error.message);
     }
-=======
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log("db connected successfully");
-   
-    
-    
-
->>>>>>> f859783cb6e3d30e571260085a8540861c3d6e75
 }
